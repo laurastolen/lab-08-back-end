e62fb52b08c4d9f9fdd9516a6c35b0083a17de3b
laura antonius <laurastolen@gmail.com>
2019-12-13 09:17:19 -0800

    working on database and refactoring

--- schema.sql
+++ schema.sql
@@ -1,9 +1,20 @@
-DROP TABLE location;
+DROP TABLE locations;
 
-CREATE TABLE IF NOT EXISTS location(
+CREATE TABLE IF NOT EXISTS locations(
   id SERIAL PRIMARY KEY,
   latitude NUMERIC(10, 7),
   longitude NUMERIC(10, 7),
+  city VARCHAR(255)
+);
+
+INSERT INTO location (latitude, longitude, city) VALUES (47.6062095, -122.3320708, 'seattle');
+
+
+DROP TABLE weather;
+
+CREATE TABLE IF NOT EXISTS weather(
+  id SERIAL PRIMARY KEY,
+  forecast VARCHAR(255),
   city VARCHAR(255)
 );
 
--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const pg = require('pg');
 const client = require('./client');
 
 const Location = require('./lib/location/Location');
@@ -30,13 +29,13 @@ app.get('/location', locationHandler);
 function locationHandler(req, res) {
   let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
-  let sql = 'SELECT * FROM location WHERE city=$1;';
+  let sql = 'SELECT * FROM locations WHERE city=$1;';
   let city = req.query.data;
   let safeValues = [city];
 
   client.query(sql, safeValues)
     .then(results => {
-      console.log(results.rows);
+      // console.log(results.rows);
       if (results.rows.length > 0) {
         return res.send(results.rows[0]);
       } else {
@@ -49,7 +48,7 @@ function locationHandler(req, res) {
             let longitude = geoData.results[0].geometry.location.lng;
 
             const locationObj = new Location(city, geoData);
-            let sql = 'INSERT INTO location (city, latitude, longitude) VALUES ($1, $2, $3);';
+            let sql = 'INSERT INTO locations (city, latitude, longitude) VALUES ($1, $2, $3);';
             let safeValues = [city, latitude, longitude];
 
             client.query(sql, safeValues);
@@ -69,6 +68,16 @@ app.get('/weather', weatherHandler);
 // weatherHandler, which contains the superagent.get, which contains .then and .catch. The .then uses the constructor to create location instances
 function weatherHandler(req, res) {
   let url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${req.query.data.latitude},${req.query.data.longitude}`;
+
+  let sql = 'SELECT * FROM locations WHERE city=$1;';
+  let city = req.query.data;
+  let safeValues = [city];
+
+  client.query(sql, safeValues)
+    .then(results => {
+      console.log(results.rows.length)
+    })
+
   superagent.get(url)
     .then(data => {
       const weatherData = data.body.daily.data.map((value) => new Weather(value.summary, value.time));
@@ -79,7 +88,7 @@ function weatherHandler(req, res) {
 
 // page not found route
 app.get('*', (req, res) => {
-  res.status(404).send('page not found!');
+  res.status(404);
 });
 
 
